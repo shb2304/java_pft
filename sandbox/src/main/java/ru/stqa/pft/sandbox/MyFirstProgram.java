897fad6e9bf267ce3a8049b10145bc5c2479f573
shb2304 <shb2304@gmail.com>
2016-09-11 19:25:46 +0300

    изменение промежуточные

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -3,7 +3,12 @@ package ru.stqa.pft.sandbox;
 public class MyFirstProgram {
 
   public static void main(String[] args) {
-    System.out.println("Hello, world!");
+    String word = "World";
+    System.out.println("Hello," + word +"!");
+
+    double l=7;
+    double s = l*l;
+    System.out.println("Площадь квадрата со стороной"+ l+"="+s);
 }
 
 }
