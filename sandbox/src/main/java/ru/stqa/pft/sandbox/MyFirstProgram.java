e3fea4cad517316cd8154f87869630d120999589
shb2304 <shb2304@gmail.com>
2016-09-13 22:52:09 +0300

    расчет расстояния между точками

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -3,12 +3,29 @@ package ru.stqa.pft.sandbox;
 public class MyFirstProgram {
 
   public static void main(String[] args) {
-    String word = "World";
-    System.out.println("Hello," + word +"!");
+    hello("world");
 
-    double l=7;
-    double s = l*l;
-    System.out.println("Площадь квадрата со стороной"+ l+"="+s);
-}
 
+
+
+  Point p1= new Point();
+  p1.x = 4;
+  p1.y = 1;
+  Point p2= new Point();
+  p2.x = 7;
+  p2.y = 5;
+    System.out.println("Расстояние между точками p1("+p1.x+";"+p1.y+")" + "и" + "p2("+p2.x+";"+p2.y+")" + "=" + distance(p1,p2));
+
+
+
+  }
+  public static void hello(String word) {
+   System.out.println("Hello," + word + "!");
+  }
+
+
+
+  public static double distance(Point p1, Point p2) {
+    return Math.sqrt (Math.pow((p2.x-p1.x),2)+Math.pow((p2.y-p1.y),2));
+  }
 }
---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -0,0 +1,9 @@
+package ru.stqa.pft.sandbox;
+
+/**
+ * Created by Sai on 13.09.2016.
+ */
+public class Point {
+  public int x;
+  public int y;
+}
