2a8d97d1d983c37d2799cb32a2477ba7fb16746e
shb2304 <shb2304@gmail.com>
2016-09-14 20:23:46 +0300

    расстояние между двумя точками

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -7,11 +7,10 @@ public class MyFirstProgram {
 
 
 
-    Point p1= new Point(4, 1);
-    Point p2= new Point(7, 5);
-    System.out.println("Расстояние между точками p1("+p1.x+";"+p1.y+")" + "и" + "p2("+p2.x+";"+p2.y+")" + "=" + distance(p1,p2));
+    Point p1= new Point(2, 0);
+    Point p2= new Point(3, 0);
 
-
+    System.out.println("Расстояние между точками p1("+p1.x+";"+p1.y+")" + "и" + "p2("+p2.x+";"+p2.y+")" + "=" + p2.distance(p1));
 
   }
   public static void hello(String word) {
@@ -20,7 +19,7 @@ public class MyFirstProgram {
 
 
 
-  public static double distance(Point p1, Point p2) {
-    return Math.sqrt (Math.pow((p2.x-p1.x),2)+Math.pow((p2.y-p1.y),2));
-  }
+  //public static double distance(Point p1, Point p2) {
+    //return Math.sqrt (Math.pow((p2.x-p1.x),2)+Math.pow((p2.y-p1.y),2));
+ // }
 }
--- sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -13,5 +13,10 @@ public class Point {
   }
 
 
+  public double distance(Point p) {
+
+    return Math.sqrt (Math.pow((this.x-p.x),2)+Math.pow((this.y-p.y),2));
+
+  }
 
 }
